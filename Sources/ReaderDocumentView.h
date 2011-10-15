b3ef25b85a82cb2d3fac69a2e39ad89b1ea420f5
Edward Rudd <urkle@outoforder.cc>
2011-10-15 11:04:44 -0400

    Export the delagate as an IBOutlet

--- Sources/ReaderDocumentView.h
+++ Sources/ReaderDocumentView.h
@@ -29,7 +29,7 @@
 // The current page that the PDF viewer is displaying.  OR assign to change pages
 @property (nonatomic,assign) NSInteger currentPage;
 // The delegate to pass of certain events too
-@property (nonatomic,assign) id<ReaderDocumentViewDelegate> delegate;
+@property (nonatomic,assign) IBOutlet id<ReaderDocumentViewDelegate> delegate;
 
 @end
 
